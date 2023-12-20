4656bae3176b8cb443801f43e47e4cdb1b677980
svanik-gantori <119661803+svanik-gantori@users.noreply.github.com>
2023-12-20 16:35:13 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,54 @@
+img="";
+status="";
+objects=[];
+
+function preload()
+{
+    img=loadImage('livingroom.avif');
+}
+
+function setup()
+{
+    canvas=createCanvas(400,380);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function draw()
+{
+    image(img,0,0,640,420);
+    if(status !="")
+    {
+        for(i=0; i<objects.length; i++)
+        {
+            document.getElementById("status").innerHTML="Objects are detected";
+            fill("red");
+            percentage=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percentage+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        }
+    }
+    
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status=true;
+    objectDetector.detect(img,gotResults)
+}
+
+function gotResults(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+
+    console.log(results); 
+    objects=results;  
+}
